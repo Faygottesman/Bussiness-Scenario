09a748f8a3ddb86401ab1bf0356eeaf37c3bccf5
Faygottesman <fgottesman123@gmail.com>
2022-01-14 13:00:30 -0500

    #21

---
+++ spec for creating the scenario.sql
@@ -0,0 +1,44 @@
+/* Hi I am the owner of a very large company and I am opening a library for the Jewish community.
+Please put together a database with data that will help this library run smoothly.
+I would like that in this data there should be the following collumns:
+LastName,
+FirstName,
+BookName,
+BookAuthor,
+BookType,
+DateTookOut,
+DateReturned,
+GoodCondition,
+TotalBalance,
+PreviousPaid
+
+Please put as many possible constraints that you can think of.
+(Don't make yourself crazy, use your commen sense.)
+If you would like, I will obviously pay you more if you test the constraints. :)
+
+Here is some real sample data:
+
+'Stay Happy', 'Libby Weiss', 'Kids', 'Lasker', 'Sarah', 08-07-2021, 08-25-2021, 1, 0, 0
+'Don''t Mess', 'Chavy Gross', 'Adult', 'Roth', 'Shira', 09-07-2021, 09-20-2021, 0, '5.00', 0
+'Sparkle', 'Dov Hirsch', 'Adult', 'Green', 'Chaim', 10-07-2020, 01-08-2021, 1, '25.00', 0
+'Middos Book', 'Rachel Heller', 'Kids', 'Freid', 'Miri', 09-07-2021, 09-28-2021, 1, 0, 0
+'Power Hungry', 'Dov Hirsch', 'Adult', 'Neiman', 'Perele', 05-15-2020, 06-24-2020, 1, 0, 0
+'Twin and Toys', 'Shira Sinsly', 'Kids', 'Lasker', 'Sarah', 08-07-2021, 08-25-2021, 1, 0, 0
+
+Please add another 3 more of your own leaving out the money part and make the date returned be a date before today.
+
+there are a few lists I would like to be able to access easily:
+
+--1: Show all but group by full name. (one collumn should be the name and the next collumn should be this list of all the books that were taken out)
+
+--2: Show a list of those that have a balance more that $20 and they don't have any books from the library.
+
+--3: Show a list of books that are available to take out and their condition should be very good.
+
+--4: Show 2 lists, one for adult books and the other kids. Please order by book name.
+
+THANK YOU SO MUCH!!!!
+
+Time and quality important so please keep that in mind as you work.
+Please keep track of how many hours you worked on it and there may be a bonus!!!
+Good luck!!!
